821d7167227b3edfbe7d04c959832e6ffc17380f
KANISORN LEE <kanisorn.lee@cmu.ac.th>
2019-01-03 17:19:22 +0700

    modified navbar and modified input fileds

--- api/document.php
+++ api/document.php
@@ -1,5 +1,19 @@
 <?php
-    $obj = new StdClass();
-    $obj->foo = "12345";
-    return json_encode($obj);
+    session_start();
+
+    if (isset($_GET['test']) == true) {
+        $obj = new StdClass();
+        $obj->foo = "12345";
+        echo json_encode($obj);
+    } elseif (isset($_POST['create_form']) == 'create') {
+        // $obj = new StdClass();
+        $_SESSION['create'][0] = "create success";
+        $_SESSION['create'][1] = "create success";
+        $_SESSION['create'][2] = "create success";
+        $arr = $_SESSION;
+        // $arr.push("create success");
+        echo json_encode($_SESSION);
+    } else {
+        echo "false";
+    }
 ?>
